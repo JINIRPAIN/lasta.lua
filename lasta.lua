d736d5378ec87ce133d6ede4d14c889c00e4ef63
JINIRPAIN <81953403+JINIRPAIN@users.noreply.github.com>
2023-10-22 18:57:19 +0300

    Update lasta.lua

--- lasta.lua
+++ lasta.lua
@@ -1,24 +1,15 @@
--- اللاعبون المحليون
+-- Local Players
 local Players = game:GetService("Players")
 
--- دالة a1
-local function a1(player)
-    -- إنشاء هايليت محلي
+-- Function to highlight a player
+local function highlightPlayer(player)
     local highlight = Instance.new("SelectionBox")
     highlight.Parent = player.Character
-    highlight.Color = Color3.new(0, 0, 255) -- لون الهايليت
-    highlight.OutlineColor = Color3.new(0, 0, 255) -- لون الإطار الخارجي للهايليت
-
-    -- دورة عبر جميع اللاعبين
-    for _, otherPlayer in pairs(Players:GetPlayers()) do
-        if otherPlayer ~= player then
-            -- تمرير كل لاعب إلى الدالة highlightPlayer
-            highlightPlayer(otherPlayer)
-        end
-    end
+    highlight.Color = Color3.fromRGB(255, 0, 0) -- Highlight color (red)
+    highlight.OutlineColor = Color3.fromRGB(0, 0, 255) -- Outline color (blue)
 end
 
--- دورة عبر جميع اللاعبين المتصلين
-for _, player in pairs(Players:GetPlayers()) do
-    a1(player)
+-- Loop through all players to highlight them
+for _, player in ipairs(Players:GetPlayers()) do
+    highlightPlayer(player)
 end
